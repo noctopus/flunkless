35a62fb801cd68a66919f530755de3d6f082231b
iteriani <fortrex@gmail.com>
2014-05-07 02:29:56 -0700

    wtf

--- public/js/services.js
+++ public/js/services.js
@@ -9,7 +9,13 @@ app.constant('geolocation_msgs', {
 });
 
 app.factory('socket', function ($rootScope) {
-  var socket = io.connect(window.location.hostname);
+
+  io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+  });
+
+  var socket = io.connect();
   if (socket.socket.connected === false) {
     $rootScope.status = 'offline';
   }
